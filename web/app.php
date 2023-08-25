ab7034b5f2f2ceeda0b5314a054ebb1f21be59e4
Mickael-Geerardyn <root@DESKTOP-D1ECK70.localdomain>
2023-08-25 14:58:07 -0400

    commit to run composer recipes:update vendor/package command

--- web/app.php
+++ web/app.php
@@ -1,13 +1,14 @@
 <?php
 
+use Composer\Autoload\ClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-/** @var \Composer\Autoload\ClassLoader $loader */
+/** @var ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+$kernel = new AppKernel('dev', true);
+//$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
