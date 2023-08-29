66f9c5a419b5f32b4e0ceeb41fc597298aa3e63f
Mickael-Geerardyn <mickael.geerardyn@gmail.com>
2023-08-29 17:08:51 -0400

    try to display homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    #[Route("/", name: "homepage")]
+	/**
+	 * @Route("/home", name="homepage")
+	 */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
